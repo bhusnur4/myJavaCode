cdbdb62378420d46424a41d4619c349134c33a9c
abhusnurmath <bhusnur@gmail.com>
2014-03-04 23:04:08 -0500

    changed some comments in Destroyer

--- battleship/Destroyer.java
+++ battleship/Destroyer.java
@@ -1,6 +1,5 @@
 package battleship;
 
-
 public class Destroyer extends Ship{
 
 	public Destroyer (int len) {
@@ -18,15 +17,17 @@ public class Destroyer extends Ship{
 	}
 	
 	@Override
-	//Returns a single-character String to use in the Ocean's print
-	//method (see below).
-	//This method should return "x" if the ship has been sunk, "S" if it has not 
-	//been sunk. This method can be used to print out locations in the ocean that
-	//have been shot at; it should not be used to print locations that have not 
-	//been shot at. 
-	//Since toString behaves exactly the same for all ship types, it can be
-	//moved into the Ship class, and simply inherited by each individual type of 
-	//ship.
+	/*
+	Returns a single-character String to use in the Ocean's print
+	method (see below).
+	This method should return "x" if the ship has been sunk, "S" if it has not 
+	been sunk. This method can be used to print out locations in the ocean that
+	have been shot at; it should not be used to print locations that have not 
+	been shot at. 
+	Since toString() behaves exactly the same for all ship types, it can be
+	moved into the Ship class, and simply inherited by each individual type of 
+	ship.
+	*/
 	public String toString() {
 		if (this.isSunk()) {
 			return "x";
